65386c201fad81e0c3a7c24887da096f80ffcbdb
Alexander Yunin <alexander.yunin@shypple.com>
2025-11-19 15:31:02 +0100

    wip

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,18 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 Bundler.require(*Rails.groups)
 
 module DreamWeaver
   class Application < Rails::Application
     config.load_defaults 7.1
-    
+
     # Configure which image generator to use
     # Options: :dalle3, :midjourney, :stability, :gemini, :placeholder
-    config.image_generator = ENV.fetch('IMAGE_GENERATOR', 'placeholder').to_sym
-    
+    config.image_generator = :gemini
+
     # Configuration for generators
-    config.autoload_paths += %W(#{config.root}/app/services)
+    config.autoload_paths += %W[#{config.root}/app/services]
   end
 end
